4e233c455c6baeaeba3981d1af6372ae3b835d0f
jpetito <jpetito@frba.utn.edu.ar>
2025-11-30 23:00:08 -0300

    sigo agregando scripts

--- worker/scripts/exec-mem-1.sh
+++ worker/scripts/exec-mem-1.sh
@@ -2,7 +2,7 @@
 
 cd "$(dirname "$0")"
 
-CMD="./../bin/worker ../configs/worker-mem.config 1"
+CMD="./../bin/worker ../configs/worker-mem-1.config 1"
 
 if command -v xfce4-terminal &> /dev/null; then
     xfce4-terminal --title="Worker" --hold --command="$CMD" &
